3324dc22ba9601504620d68e0999c8222348bc6f
des12589 <d.makarov44@yandex.ru>
2024-09-22 18:58:21 +0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
+}
